62b3b7872f1c564677b1122e6a17c3cc6726ccfd
Jennifer Shiau <jennifershiau@Jenniferde-MacBook-Pro.local>
2019-05-30 16:52:17 +0800

    demo code

---
+++ demo.py
@@ -0,0 +1,25 @@
+
+
+
+
+###########################
+# This is a code for demo #
+###########################
+
+print('\n=============================')
+print('= Intro to Computer Network =')
+print('=============================\n')
+
+print('Demo ...\n')
+
+for i in range(10):
+    for j in range(i):
+        print('*', end='')
+    print()
+
+#############
+# Type here #
+#############
+'''
+
+'''
